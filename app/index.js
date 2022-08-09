2adb090eec4277cc7ee4f0c1c748195a6228e134
Harikrishnan <foslux@gmail.com>
2022-08-09 23:09:48 +0530

    user route add

--- app/index.js
+++ app/index.js
@@ -6,4 +6,8 @@ app.use(cors());
 import router from "./routes/route.allProduct.js";
 
 app.use("/api", router);
+
+app.post('/api/auth/login',(req,res)=>{
+    console.log(res);
+})
 export default app;
