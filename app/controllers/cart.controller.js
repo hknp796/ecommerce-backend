7d60faf001e2f45243af84fdd2f622ad3b5c6496
Harikrishnan <hknp796@gmail.com>
2022-09-17 14:52:00 +0530

    cart change to id

--- app/controllers/cart.controller.js
+++ app/controllers/cart.controller.js
@@ -2,16 +2,12 @@ import { cartItems } from "../models/cartItems.model.js";
 
 const cartData = async (req, res) => {
   try {
-    console.log(req.body, "headers");
-
-    req.body.product_id = req.body._id;
     req.body.user_id = req.user.user._id;
     delete req.body._id;
+    console.log(req.body, "headers");
 
-    const cart = await cartItems.create({
-      ...req.body,
-    });
-    console.log({ cart });
+    const cart = await cartItems.create(req.body);
+
     return res.status(200).json(cart);
   } catch (e) {
     return res.status(500).json({ message: e.message, status: "Failed" });
--- app/models/cartItems.model.js
+++ app/models/cartItems.model.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 const cartItem = mongoose.Schema({
-  name: String,
-  brand: String,
-  gender: String,
-  category: String,
-  price: Number,
-  img: Array,
   product_id: String,
   user_id: String,
 });
