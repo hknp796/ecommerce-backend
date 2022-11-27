f600c61a4eac89f5469c426464614fb4255b4de9
Harikrishnan <foslux@gmail.com>
2022-11-27 20:24:09 +0530

    adding heroku

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 import app from "./index.js";
 import connect from "./config/db.js";
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, async () => {
   await connect();
   console.log(`Listening to port ${port}`);
