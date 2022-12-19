f769839a416eb7fdd6fd68927b8ca62b9f0c6a88
Harikrishnan <foslux@gmail.com>
2022-12-19 22:10:06 +0530

    cors update

--- app/index.js
+++ app/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 import router from "./routes/route.allProduct.js";
