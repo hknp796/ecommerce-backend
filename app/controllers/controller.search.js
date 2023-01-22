364cd194017dfb3676a6a3dca7863d9ccc6e5554
Harikrishnan <hknp796@gmail.com>
2023-01-22 12:38:51 +0530

    filter and search add

--- app/controllers/controller.search.js
+++ app/controllers/controller.search.js
@@ -2,17 +2,18 @@ import { allProducts, searches } from "../models/allProducts.js";
 
 const search = async (req, res) => {
   try {
-    console.log(req.body);
     let search = req.body.search;
-    // let search = new RegExp(req.body.search, "i");
-    // let search = new RegExp(req.body.search, "i");
 
-    const items = await allProducts.aggregate([
-      { $match: { color: { $in: search } } },
+    await allProducts.collection.createIndex({
+      gender: "text",
+      color: "text",
+      size: "text",
+      category: "text",
+    });
 
-      // { $match: { size: { $in: search } } },
-    ]);
-
+    const items = await allProducts.find({
+      $text: { $search: search.join(" ") },
+    });
     res.send(items);
   } catch (e) {
     return res.status(500).json({ message: e.message, status: "Failed" });
